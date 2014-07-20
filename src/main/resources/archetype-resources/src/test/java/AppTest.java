3180795d39e435e91f6845a498b5158a7256949a
Antoine MICHAUD <antoinemichaud34@gmail.com>
2014-07-20 10:47:06 +0200

    initial commit

---
+++ src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -0,0 +1,16 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        assertThat(true).isTrue();
+    }
+}
